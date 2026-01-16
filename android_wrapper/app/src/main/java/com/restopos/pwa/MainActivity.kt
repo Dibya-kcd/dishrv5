e357842770fc8da24eed2617dddd66278f40eb6c
Dibya-kcd <dibyaprakashkcd@gmail.com>
2026-01-16 11:59:57 +0530

    ok

---
+++ android_wrapper/app/src/main/java/com/restopos/pwa/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.restopos.pwa
+
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private val requestPerms = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { _ -> }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ensureBtPermissions()
+        webView = findViewById(R.id.webview)
+        val s: WebSettings = webView.settings
+        s.javaScriptEnabled = true
+        s.domStorageEnabled = true
+        s.allowFileAccess = true
+        s.loadsImagesAutomatically = true
+        s.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = WebViewClient()
+        webView.addJavascriptInterface(PrinterBridge(this, webView), "AndroidPrinter")
+        val url = "https://dibya-kcd.github.io/dishrv5/"
+        webView.loadUrl(url)
+    }
+
+    private fun ensureBtPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestPerms.launch(arrayOf(Manifest.permission.BLUETOOTH_CONNECT))
+        }
+    }
+}
